6eec3c26d5306bf8baf9dec1e3b462b8d64b0eae
jdios <jdios@jdiosui-MacBookAir.local>
2025-05-21 14:25:59 +0900

    커스텀 셀 뷰 완성

--- SlowStarter/SlowStarter/Sources/Presentation/Scenes/RepetitiveStudyScene/AssignmentTableViewCell.swift
+++ SlowStarter/SlowStarter/Sources/Presentation/Scenes/RepetitiveStudyScene/AssignmentTableViewCell.swift
@@ -14,15 +14,15 @@ class AssignmentTableViewCell: UITableViewCell {
     
     private let assignmentImageView: UIImageView = {
         let imgView = UIImageView()
+        imgView.image = UIImage(systemName: "person.crop.square.on.square.angled.fill")
         imgView.contentMode = .scaleAspectFit
-        imgView.translatesAutoresizingMaskIntoConstraints = false
         imgView.clipsToBounds = true
         return imgView
     }()
     
     private let memoLabel: UILabel = {
         let label = UILabel()
-        label.text = "메모메모"
+        label.text = "칼질할 때는 손을 오므리고 두번째 마디에 칼 옆면을 대도록 할것"
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 14)
         
@@ -31,15 +31,15 @@ class AssignmentTableViewCell: UITableViewCell {
     
     private let celltitleLabel: UILabel = {
         let label = UILabel()
+        label.text = "번째 인증"
         label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         return label
     }()
     
-    private let submitButton: UIButton = {
-       let button = UIButton()
+    private let addPhotoButton: UIButton = {
+        let button = UIButton()
         button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
-        button.setTitle("과제 제출하기", for: .normal)
-        button.backgroundColor = .green
+        button.setTitle("사진 추가하기", for: .normal)
         return button
     }()
     
@@ -57,17 +57,52 @@ class AssignmentTableViewCell: UITableViewCell {
         // Initialization code
     }
     
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     private func setupUI() {
+        
+        contentView.addSubview(celltitleLabel)
+        celltitleLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(10)
+        }
+        contentView.addSubview(addPhotoButton)
+        addPhotoButton.snp.makeConstraints { make in
+            make.top.equalTo(celltitleLabel).offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+            
+        }
+        contentView.addSubview(assignmentImageView)
         assignmentImageView.snp.makeConstraints { make in
-            <#code#>
+            make.top.equalTo(celltitleLabel.snp.bottom).offset(20)
+            make.width.height.equalTo(50)
+        }
+        contentView.addSubview(memoLabel)
+        memoLabel.snp.makeConstraints { make in
+            make.top.equalTo(assignmentImageView.snp.bottom).offset(12)
+            make.leading.equalToSuperview().offset(10)
         }
     }
+    // 셀에 데이터를 채우는 메서드
+    public func configure(with assignment: Assignment) {
+        memoLabel.text = assignment.memo
+        assignmentImageView.image = assignment.image
+    }
+    
+    // 셀이 재사용될 때 호출되어 이전 데이터를 초기화 (선택적)
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        memoLabel.text = nil
+        assignmentImageView.image = nil
+    }
+    
+}
 
+
+#Preview {
+    AssignmentTableViewCell()
 }
