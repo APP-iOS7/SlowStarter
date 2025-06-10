04ac4f556dc1c712226e8a427c9cd9896a41d126
yohans <rhdygks231@naver.com>
2025-06-10 09:21:26 +0900

    fix

--- SlowStarter/SlowStarter/Sources/Presentation/Coordinators/MainCoordinator.swift
+++ SlowStarter/SlowStarter/Sources/Presentation/Coordinators/MainCoordinator.swift
@@ -62,16 +62,16 @@ class MainCoordinator: NSObject, Coordinator {
         appearance.backgroundColor = .white
         
         // 선택된 탭 바 아이템 색상
-        appearance.stackedLayoutAppearance.selected.iconColor = .systemCyan
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(hex: "#442C2E") ?? .black
         appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
-            .foregroundColor: UIColor.systemRed,
+            .foregroundColor: UIColor(hex: "#442C2E") ?? .black,
             .font: UIFont.systemFont(ofSize: 12, weight: .semibold)
         ]
         
         // 선택되지 않은 아이템 색상
         appearance.stackedLayoutAppearance.normal.iconColor = .lightGray
         appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
-            .foregroundColor: UIColor.lightGray,
+            .foregroundColor: UIColor(hex: "#442C2E") ?? .black,
             .font: UIFont.systemFont(ofSize: 12, weight: .regular)
         ]
         
